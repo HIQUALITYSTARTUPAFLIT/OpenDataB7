b4c00703235d58595b58cd3dea84e27590d77255
danhab99 <dan.habot@gmail.com>
2018-01-04 11:11:57 +0200

    Updated read me

--- OpenDataB7/server.js
+++ OpenDataB7/server.js
@@ -3,6 +3,6 @@ var http = require('http');
 var port = process.env.PORT || 1337;
 
 http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify(req.headers));
 }).listen(port);
